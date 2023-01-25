a3a208361b723c8cdbdb70a5f9fd0da3878c5418
ahmad raze <ahmad raze>
2023-01-25 22:46:32 +0800

    your msg

---
+++ application/controllers/admin/Menu.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script accsess allowed');
+
+class Menu extends CI_Controller {
+    public function index()
+    {
+        $data = array(
+            'content'=> 'dashboard/index.php'
+        );
+        $this->load->view('template/menu',$data);
+    }
+}
